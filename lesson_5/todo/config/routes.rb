5e26eac3b824826e6e8da189b8b03ef9ef5e8199
Chinderbember <chinderbember@yahoo.com>
2022-08-21 22:59:12 +0300

    added lesson_8 lecture_work

--- lesson_5/todo/config/routes.rb
+++ lesson_5/todo/config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'about', to: 'about#index'
+  resources :events
+  get 'about', to: 'about#index', as: 'about_index'
+  get 'about/:hello', to: 'about#index', additional: 'true'
   root 'events#index'
-  get 'help', to: 'about#help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
